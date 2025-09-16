c10e39c7c87fd87cefd3266a35eb533ecc0715de
moneda100stud <al23760824@ite.edu.mx>
2025-09-15 21:12:40 -0700

    Update practica1_figurasG.py
    actualizando código en la  parte de la linea recta además hacer correcciones de módulos

--- Practica-1/practica1_figurasG.py
+++ Practica-1/practica1_figurasG.py
@@ -1,5 +1,18 @@
-import turtle as t
-import tkinter as tk 
-t = t.Turtle()
-t.forward(100)
-t.right(90)
+"""importamos la libreria turtle"""
+import turtle as t 
+
+"""aqui realizamos una linea recta de 100 pixeles sencillo """
+#t.forward(100)
+#t.right(90)
+"""para hacer un recta con color tenemos que aplicar un def draw_line """
+def draw_line(color,length,start_x,start_y):
+    """hacemos de pen un objeto para poder evitar conflictos de modulos como t turtle"""
+    pen=t.Turtle()
+    pen.penup()
+    pen.goto(start_x,start_y)
+    pen.pendown()
+    pen.pencolor(color)
+    pen.forward(length)
+if __name__=="__main__":
+    draw_line("red",100,0,0)
+t.done() 
