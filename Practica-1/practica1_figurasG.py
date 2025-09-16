969981a47da70957ab7e69134e869e8b0210da0a
moneda100stud <al23760824@ite.edu.mx>
2025-09-15 22:17:32 -0700

    Update practica1_figurasG.py
    por ultimo agregamos la figurar circulo para realizar la misma metodologia comentamos las figuras anteriores para realizar el uso del circulo sin conflictos  y asi agregar  la ultima figurar documentada

--- Practica-1/practica1_figurasG.py
+++ Practica-1/practica1_figurasG.py
@@ -33,17 +33,30 @@ import turtle as t
 #t.done()
 """realizaremos el bloque de la figura del triangulo con turtle"""""
 """en esta parte se creo el def para hacer mencion de la figura del triangulo"""
-def draw_triangle(color,length,start_x,start_y):
-    pen=t.Turtle()
-    pen.penup()
-    pen.goto(start_x,start_y)
-    pen.pendown()
-    pen.pencolor(color)
-    """en esta seccion del bloque se realizar un for para repetir el la recta hasta dar con el angulo de 120 grados y asi realizar el triangulo """
-    for i in range(3):
-        pen.forward(length)
-        pen.right(120)
+#def draw_triangle(color,length,start_x,start_y):
+   # pen=t.Turtle()
+    #pen.penup()
+    #pen.goto(start_x,start_y)
+    #pen.pendown()
+    #pen.pencolor(color)
+"""en esta seccion del bloque se realizar un for para repetir el la recta hasta dar con el angulo de 120 grados y asi realizar el triangulo """
+    #for i in range(3):
+        #pen.forward(length)
+        #pen.right(120)
 """mencionamos el codigo principal main para realizar correctamente el codigo y asi evitar conflictos de modulos tambien cambiamos los color a verde para marcar la diferencia de figuras"""
+#if __name__=="__main__":
+    #draw_triangle("green",100,0,0)
+#t.done() # t.done nos sirve crucialmente para mantener la ventana del tkinter abierto y correr el codigo correctamente
+"""por ultimo crear la figurar circulo procediendo a realizar un def_circle"""
+def draw_circle(color,radius,start_x,start_y):
+    """realizamos el mismo procedimiento como las anteriores figuras """
+    pen=t.Turtle() #aqui declaramos el objeto a llamar pen 
+    pen.penup() #se levantar el lapiz del turtle 
+    pen.goto(start_x,start_y) # el uso de las coordenas x, y,
+    pen.pendown() # se bajar el lapiz del turtle 
+    pen.pencolor(color) # realizar el color destinado o mencionado en los parametros 
+    pen.circle(radius) # se utilizar el codigo del radios haciendo un llamado a que haga referencia al radio de un circulo 
+"""se agregar el uso del if para mencionar el archivo main py y llenar los parametros del circulo y el color elegido """
 if __name__=="__main__":
-    draw_triangle("green",100,0,0)
-t.done() # t.done nos sirve crucialmente para mantener la ventana del tkinter abierto y correr el codigo correctamente
+    draw_circle("yellow",100,0,-70)
+t.done()
